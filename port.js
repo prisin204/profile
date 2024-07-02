dbd4b9baa145890104f7a71de61f901f62d455ac
fokla <hhajriwnko@gmain.com>
2024-07-02 21:12:01 +0530

    animation

--- port.js
+++ port.js
@@ -30,3 +30,59 @@ $(document).ready(function(){
 
 var contentBox = document.querySelector('.fourthBox');
 var btn = document.querySelectorAll('button-62');
+var targetElement = document.querySelector('.targetName');
+
+function textSplitter(targetElement){
+    
+    var target = document.querySelector(targetElement);
+    var tContent = target.textContent;
+    var splitedText = tContent.split("");
+    var clutter="";
+    var classNum = "";
+    var counter = 0;
+    var lenth = splitedText.length;
+    var half =  Math.floor(lenth/2);
+    
+    
+    //loop to add span to each element of target ;
+    splitedText.forEach(function (item) {
+        if(counter < half){
+            classNum = "a";
+            counter++;
+        }else{
+            classNum ="b";
+        }
+        clutter +=`<span class=${classNum}>${item}</span>`;
+    //to set the span added elements on the html page ;
+    target.innerHTML= clutter;
+    });
+    }
+    textSplitter(".targetName"); //put targeted element and get the splitted elements;
+    
+    //here we will going to add gasp on elements;
+    
+    gsap.from(".targetName .a",{
+        y:100,  //set the span display inline block from css ,because the transform properties are not applicable for inline elements;
+        opacity:0,
+        duration:1,
+        stagger:.2,
+        delay:0.1,
+        ease:"bounce",
+    })
+    gsap.from(".targetName .b",{
+        y:100,  //set the span display inline block from css ,because the transform properties are not applicable for inline elements;
+        opacity:0,
+        duration:1,
+        stagger:-0.2,
+        delay:0.1,
+        ease:"bounce"
+    })
+
+    //animation on photo
+
+    gsap.from(".proPhoto",{
+    duration: 1,
+    opacity: 0,
+    scale:.3,
+    ease:"bounce",
+    })
